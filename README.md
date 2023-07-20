b22c91db9b44041672407fb391f487e79a451bde
kevinlem0n <126497052+kevin-lem0n@users.noreply.github.com>
2023-07-20 15:42:39 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# RazorJS
+<br/>
+JavaScript library for Razor (@) templating.
+<br/>
+
+# At a glance
+
+**Razor Data Binding** <br>
+```html
+<h1>@color</h1> <!-- Razor @variable will be rendered as <h1>blue</h1> -->
+
+<script>
+    var color = "blue"; // set the value variable "color"
+</script>
+```
---
+++ Razor.js
@@ -0,0 +1,16 @@
+/*
+RazorJS - JavaScript library for Razor (@) templating.
+Credits - This project is inspired by Microsoft ASP .NET Razor syntax.
+
+> IMPORTANT: Razor.js have to be LOADED first before your actual <script> tag.
+
+> READ ME BEFORE USING
+RazorJS IS NOT compatible with whyjs library.
+
+Created by: Kevin (https://github.com/kevin-lem0n)
+*/
+(function(b){b.a=function(){var a=document.body.innerHTML;
+var rc=a.replace(/@(\w+)/g,function(m,c){
+if(window.hasOwnProperty(c)){return window[c];} 
+else{return m;}});document.body.innerHTML=rc;};})(window);
+document.addEventListener("DOMContentLoaded",function(){a();});
